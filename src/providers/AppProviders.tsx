e7b9883a341ce8669fd5209faf48980e6b9f2658
dfenter-dot <dfenter@quad2electric.com>
2026-01-24 11:17:43 -0600

    Update AppProviders.tsx

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,16 +1,6 @@
 import { ReactNode } from 'react';
-import { AuthProvider } from './auth/AuthContext';
-import { DataProvider } from './data/DataContext';
-import { SelectionProvider } from './selection/SelectionContext';
+import { DialogProvider } from './dialogs/DialogContext';
 
 export function AppProviders({ children }: { children: ReactNode }) {
-  return (
-    <AuthProvider>
-      <DataProvider>
-        <SelectionProvider>
-          {children}
-        </SelectionProvider>
-      </DataProvider>
-    </AuthProvider>
-  );
+  return <DialogProvider>{children}</DialogProvider>;
 }
